812b2144e64a27d65181a435d6e4648a1cd92ee0
omrivilan <136691490+omrivilan@users.noreply.github.com>
2024-07-16 17:02:59 +0300

    Update App.py

--- App.py
+++ App.py
@@ -273,7 +273,7 @@ def plot_relative_crime_by_religion_and_group(df, data, selected_group):
                       legend_title=dict(font=dict(size=20, color="white")),
                       yaxis_title=dict(
                           font=dict(size=20, color="white"),  # Increase the text size
-                          standoff=100
+                          standoff=200
                       ),
                       yaxis=dict(
                           autorange='reversed'  # Reverse the y-axis order
